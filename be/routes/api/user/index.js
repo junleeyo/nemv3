dbe058442543a12a2201b6dfa4d23c1eb2b6372a
junleeyo <lcj715@korea.com>
2020-11-12 07:33:51 +0000

    basic study 20201112

--- be/routes/api/user/index.js
+++ be/routes/api/user/index.js
@@ -1,33 +1,81 @@
 var express = require('express');
 var createError = require('http-errors');
 var router = express.Router();
-
-const us =[
-    {
-        name: '김김김',
-        age: 14
-    },
-    {
-        name: '이이이',
-        age: 24
-    }
-]
+const User = require('../../../models/users')
 
 router.get('/', function(req, res, next) {
-    res.send({users: us});
+    User.find()
+        .then(r => {
+            res.send({ success: true, users: r })
+        })
+        .catch(e => {
+            res.send({ success: false })
+        })
 });
 
-router.post('/', function(req, res, next) {
-    res.send({success: "true"});
+router.post('/', (req, res, next) => {
+    const { name, age } = req.body
+    const u = new User({ name, age })
+    u.save()
+        .then(r => {
+            res.send({ success: true, msg: r })
+        })
+        .catch(e => {
+            res.send({ success: false, msg: e.message })
+        })
+})
+
+router.put('/:id', (req, res, next) => {
+    const id = req.params.id
+    const { name, age } = req.body
+    User.updateOne({ _id: id }, { $set: { name, age }})
+        .then(r => {
+            res.send({ success: true, msg: r })
+        })
+        .catch(e => {
+            res.send({ success: false, msg: e.message })
+        })
+    // res.send({ success: true, msg: 'put ok' })
+})
+
+router.delete('/:id', (req, res, next) => {
+    const id = req.params.id
+    User.deleteOne({ _id: id })
+        .then(r => {
+            res.send({ success: true, msg: r })
+        })
+        .catch(e => {
+            res.send({ success: false, msg: e.message })
+        })
+    res.send({ success: true, msg: 'del ok' })
+})
+
+/*
+router.get('/', function(req, res, next) {
+    console.log(req.query)
+    console.log(req.body)
+
+    res.send({ users: us })
 });
 
-router.put('/', function(req, res, next) {
-    res.send({success: "true"});
+router.post('/', (req, res, next) => {
+    console.log(req.query)
+    console.log(req.body)
+    res.send({ success: true, msg: 'post ok' })
 });
 
-router.delete('/', function(req, res, next) {
-    res.send({success: "true"});
+router.put('/', (req, res, next) => {
+    console.log(req.query)
+    console.log(req.body)
+    res.send({ success: true, msg: 'put ok' })
 });
+
+router.delete('/', (req, res, next) => {
+    console.log(req.query)
+    console.log(req.body)
+    res.send({ success: true, msg: 'del ok' })
+});
+ */
 
 
 router.all('*', function(req, res, next) {
--- fe/app.js
+++ fe/app.js
@@ -3,14 +3,27 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var cors = require('cors');
+var cors = require('cors')
 var app = express();
+var mongoose = require('mongoose');
 
-const mongoose = require('mongoose')
+var userSchema = new mongoose.Schema({
+  name: { type: String, default: '', unique: true, index: true },
+  age: { type: Number, default: 1 }
+})
+var User = mongoose.model('User', userSchema)
 
 mongoose.connect('mongodb://localhost:27017/nemv', { useNewUrlParser: true }, (err) => {
   if (err) return console.error(err)
   console.log('mongoose connected!')
+
+  User.create({ name: '하하' })
+      .then(r => console.log(r))
+      .catch(e => console.error(e))
+
+  /*User.find()
+      .then(r => console.log(r))
+      .catch(e => console.error(e))*/
 })
 
 app.use(logger('dev'));
@@ -18,8 +31,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(cors());
-app.use('/api', require('./routes/api'));
+app.use(cors())
+app.use('/api', require('./routes/api'))
 app.use(express.static(path.join(__dirname, '../', 'fe', 'dist')));
 
 // catch 404 and forward to error handler
